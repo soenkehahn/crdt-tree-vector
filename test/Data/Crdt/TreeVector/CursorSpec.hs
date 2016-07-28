9398c71fc7c80bf9f6a90e507e1608c48282c49c
Sönke Hahn <soenkehahn@gmail.com>
2016-07-28 19:17:22 +0200

    tests: use a smaller maximal size for property tests

--- test/Data/Crdt/TreeVector/CursorSpec.hs
+++ test/Data/Crdt/TreeVector/CursorSpec.hs
@@ -6,93 +6,95 @@ import           Control.DeepSeq
 import           Control.Exception
 import           Data.List
 import           Test.Hspec
+import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 
 import           Data.Crdt.TreeVector
 import           Data.Crdt.TreeVector.Cursor
-import           Data.Crdt.TreeVector.Pretty
 import           Data.Crdt.TreeVector.InternalSpec (mkClient)
+import           Data.Crdt.TreeVector.Pretty
 
 spec :: Spec
 spec = do
-  describe "Cursor" $ do
-    it "stays the same when nothing gets merged" $
-      forAllDocs $ \ doc ->
-      forAllIndices doc $ \ index ->
-        let cursor = toCursor doc index
-            newIndex = fromCursor doc cursor in
-        counterexample (show cursor) $
-        newIndex === index
+  modifyMaxSize (const 50) $ do
+    describe "Cursor" $ do
+      it "stays the same when nothing gets merged" $
+        forAllDocs $ \ doc ->
+        forAllIndices doc $ \ index ->
+          let cursor = toCursor doc index
+              newIndex = fromCursor doc cursor in
+          counterexample (show cursor) $
+          newIndex === index
 
-    context "when an element gets inserted" $ do
-      it "moves the index on an earlier insert" $ do
-        let original = "foobar"
-            doc = mkPatch (mkClient 0) mempty original
+      context "when an element gets inserted" $ do
+        it "moves the index on an earlier insert" $ do
+          let original = "foobar"
+              doc = mkPatch (mkClient 0) mempty original
+              cursor = toCursor doc 3
+              newDoc = mkPatch (mkClient 0) doc "fxoobar"
+              newCursor = fromCursor newDoc cursor
+          newCursor `shouldBe` 4
+
+        it "retains the index on a later insert" $ do
+          let original = "foobar"
+              doc = mkPatch (mkClient 0) mempty original
+              cursor = toCursor doc 3
+              newDoc = mkPatch (mkClient 0) doc "foobaxr"
+              newCursor = fromCursor newDoc cursor
+          newCursor `shouldBe` 3
+
+        it "retains the index if an element gets inserted at the cursor's position" $ do
+          let original = "foobar"
+              doc = mkPatch (mkClient 0) mempty original
+              cursor = toCursor doc 3
+              newDoc = mkPatch (mkClient 1) doc "fooxbar"
+              newCursor = fromCursor newDoc cursor
+          newCursor `shouldBe` 4 -- fixme
+
+      context "when an element gets deleted" $ do
+        it "gets decreased on earlier deletions" $ do
+          let original = "fooxbar"
+              doc = mkPatch (mkClient 0) mempty original
+              cursor = toCursor doc 5
+              newDoc = mkPatch (mkClient 0) doc "foobar"
+              newCursor = fromCursor newDoc cursor
+          newCursor `shouldBe` 4
+
+        it "stays the same on later deletions" $ do
+          let original = "fooxbar"
+              doc = mkPatch (mkClient 0) mempty original
+              cursor = toCursor doc 1
+              newDoc = mkPatch (mkClient 0) doc "foobar"
+              newCursor = fromCursor newDoc cursor
+          newCursor `shouldBe` 1
+
+        it "works when it points to an element that gets deleted" $ do
+          let original = "fooxbar"
+              doc = mkPatch (mkClient 0) mempty original
+              cursor = toCursor doc 3
+              newDoc = mkPatch (mkClient 0) doc "foobar"
+              newCursor = fromCursor newDoc cursor
+          newCursor `shouldBe` 3
+
+      it "allows to set the cursor for an empty document" $ do
+        let empty = mempty :: TreeVector Int Char
+            cursor = toCursor empty 0
+        fromCursor empty cursor `shouldBe` 0
+
+      it "allows to set the cursor after the last element" $ do
+        let doc = mkPatch (mkClient 0) mempty "foo"
             cursor = toCursor doc 3
-            newDoc = mkPatch (mkClient 0) doc "fxoobar"
-            newCursor = fromCursor newDoc cursor
-        newCursor `shouldBe` 4
+        fromCursor doc cursor `shouldBe` 3
 
-      it "retains the index on a later insert" $ do
-        let original = "foobar"
-            doc = mkPatch (mkClient 0) mempty original
-            cursor = toCursor doc 3
-            newDoc = mkPatch (mkClient 0) doc "foobaxr"
-            newCursor = fromCursor newDoc cursor
-        newCursor `shouldBe` 3
+      it "allows to set the cursor before the first element" $ do
+        let doc = mkPatch (mkClient 0) mempty "foo"
+            cursor = toCursor doc 0
+        fromCursor doc cursor `shouldBe` 0
 
-      it "retains the index if an element gets inserted at the cursor's position" $ do
-        let original = "foobar"
-            doc = mkPatch (mkClient 0) mempty original
-            cursor = toCursor doc 3
-            newDoc = mkPatch (mkClient 1) doc "fooxbar"
-            newCursor = fromCursor newDoc cursor
-        newCursor `shouldBe` 4 -- fixme
-
-    context "when an element gets deleted" $ do
-      it "gets decreased on earlier deletions" $ do
-        let original = "fooxbar"
-            doc = mkPatch (mkClient 0) mempty original
-            cursor = toCursor doc 5
-            newDoc = mkPatch (mkClient 0) doc "foobar"
-            newCursor = fromCursor newDoc cursor
-        newCursor `shouldBe` 4
-
-      it "stays the same on later deletions" $ do
-        let original = "fooxbar"
-            doc = mkPatch (mkClient 0) mempty original
-            cursor = toCursor doc 1
-            newDoc = mkPatch (mkClient 0) doc "foobar"
-            newCursor = fromCursor newDoc cursor
-        newCursor `shouldBe` 1
-
-      it "works when it points to an element that gets deleted" $ do
-        let original = "fooxbar"
-            doc = mkPatch (mkClient 0) mempty original
-            cursor = toCursor doc 3
-            newDoc = mkPatch (mkClient 0) doc "foobar"
-            newCursor = fromCursor newDoc cursor
-        newCursor `shouldBe` 3
-
-    it "allows to set the cursor for an empty document" $ do
-      let empty = mempty :: TreeVector Int Char
-          cursor = toCursor empty 0
-      fromCursor empty cursor `shouldBe` 0
-
-    it "allows to set the cursor after the last element" $ do
-      let doc = mkPatch (mkClient 0) mempty "foo"
-          cursor = toCursor doc 3
-      fromCursor doc cursor `shouldBe` 3
-
-    it "allows to set the cursor before the first element" $ do
-      let doc = mkPatch (mkClient 0) mempty "foo"
-          cursor = toCursor doc 0
-      fromCursor doc cursor `shouldBe` 0
-
-    it "is total" $
-      forAllDocs $ \ a ->
-      forAllDocs $ \ b -> \ index -> do
-        evaluate $ rnf $ fromCursor b $ toCursor a index
+      it "is total" $
+        forAllDocs $ \ a ->
+        forAllDocs $ \ b -> \ index -> do
+          evaluate $ rnf $ fromCursor b $ toCursor a index
 
 forAllDocs :: Testable t => (TreeVector Int Char -> t) -> Property
 forAllDocs prop = property $ \ (vectors :: [String]) ->
